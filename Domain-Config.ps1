dd2feb87f90afa103b12c04ec9bb037302d544c8
TToulouse <2134063@cegepba.qc.ca>
2023-09-26 14:39:52 -0400

    modif

--- Domain-Config.ps1
+++ Domain-Config.ps1
@@ -2,8 +2,13 @@ param (
     [switch]$CreateDomain = $false,
     [string]$DomainName = "novatech.local",
     [string]$DomainNetbiosName = "NOVATECH",
-    [string]$SafeModeAdminPassword = "MotDePasseAdmin",
-    [switch]$JoinToDomain = $false
+    [string]$SafeModeAdminPassword = "Test1234",
+    [switch]$JoinToDomain = $false,
+    [switch]$ConfigureClient = $false,
+    [string]$DnsServer = "8.8.8.8",
+    [string]$ComputerName = "WIN10",
+    [switch]$Help = $false
+
 )
 
 if ($CreateDomain) {
@@ -11,7 +16,7 @@ if ($CreateDomain) {
     Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
     
-    Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainNetbiosName -DomainMode WinThreshold -ForestMode WinThreshold -InstallDns -Force# -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $SafeModeAdminPassword -Force) -Force
+    Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainNetbiosName -DomainMode WinThreshold -ForestMode WinThreshold -InstallDns -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $SafeModeAdminPassword -Force) -Force
     
     Restart-Computer -Force
 }
@@ -20,8 +25,32 @@ if ($JoinToDomain) {
     $Credential = Get-Credential
     Add-Computer -DomainName $DomainName -Credential $Credential
 
-    # Restart the Windows 10 machine
+   
     Restart-Computer -Force
+}
+
+if ($ConfigureClient) {
+    # Configure le client pour utiliser le DNS du serveur
+
+    $InterfaceIndex = (Get-NetAdapter).InterfaceIndex
+    Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses ($DnsServer)
+    # join domain
+    $Credential = Get-Credential
+    Add-Computer -DomainName $DomainName -Credential $Credential
+    Restart-Computer -Force
+
+}
+
+if ($Help){
+    Write-Host "CreateDomain : Create a new domain"
+    Write-Host "DomainName : Name of the domain"
+    Write-Host "DomainNetbiosName : Netbios name of the domain"
+    Write-Host "SafeModeAdminPassword : Password of the domain"
+    Write-Host "JoinToDomain : Join the computer to the domain"
+    Write-Host "ConfigureClient : Configure the client"
+    Write-Host "DnsServer : DNS server"
+    Write-Host "ComputerName : Name of the computer"
+    Write-Host "Help : Display help"
 }
 
 else {
