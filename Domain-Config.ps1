5dd9ef9cfcbc6cd0b6f08b58fc5106cf8109a2e3
TToulouse <2134063@cegepba.qc.ca>
2023-09-26 12:10:03 -0400

    modif

--- Domain-Config.ps1
+++ Domain-Config.ps1
@@ -11,8 +11,7 @@ if ($CreateDomain) {
     Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
     
-    Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainNetbiosName -DomainMode Win2016 -ForestMode Win2016 -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $SafeModeAdminPassword -Force) -Force
-
+    Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainNetbiosName -DomainMode WinThreshold -ForestMode WinThreshold -InstallDns -Force# -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $SafeModeAdminPassword -Force) -Force
     
     Restart-Computer -Force
 }
